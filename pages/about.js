7d06e69898507991a39e78b3f9c313fe49e2feb2
Clavier Zhang <y2595zha@edu.uwaterloo.ca>
2018-06-28 12:56:30 +0800

    with redux

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
+import Layout from '../components/MyLayout.js'
+
 export default () => (
-    <div>
+    <Layout>
         <p>This is the about page</p>
-    </div>
+    </Layout>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
+import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 
-const Index = () => (
-    <div>
-        <Link href="/about" style={{ fontSize: 20 }}>
-            <a>About Page</a>
+const PostLink = (props) => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
         </Link>
-        <p>Hello Next.js</p>
-    </div>
+    </li>
 )
 
-export default Index
+export default () => (
+    <Layout>
+        <h1>My Blog</h1>
+        <ul>
+            <PostLink title="Hello Next.js" />
+            <PostLink title="Learn Next.js is awesome" />
+            <PostLink title="Deploy apps with Zeit" />
+        </ul>
+    </Layout>
+)
