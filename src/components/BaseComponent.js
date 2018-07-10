aa6686b7ecbd543bbd822453eb54b0ce4959cda2
Clavier Zhang <y2595zha@edu.uwaterloo.ca>
2018-07-10 22:28:52 +0800

    merge

--- src/components/BaseComponent.js
+++ src/components/BaseComponent.js
@@ -8,7 +8,7 @@ export class BaseComponent extends Component {
     
 
     post = (url, form) => {
-        return fetch(url, { method: 'POST', body: form, mode: 'cors'})
+        return fetch(url, { method: 'POST', body: form})
             .then((response) => (response.json()))
             .catch((error) => { console.error(error); });
     }
