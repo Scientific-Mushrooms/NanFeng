21855e60ec8e912ca22d81853b02d42e2b8b7e72
Clavier Zhang <y2595zha@edu.uwaterloo.ca>
2018-07-18 16:43:10 +0800

    date picker

--- src/screens/course/courseCreate.js
+++ src/screens/course/courseCreate.js
@@ -1,21 +1,58 @@
 import React, { Component } from "react";
 import { BaseComponent } from '../../components/BaseComponent';
-import { Divider, Grid, Button, Typography, Input, TextField} from '@material-ui/core';
+import { Divider, Grid, Button, Typography, Input, TextField, Popover} from '@material-ui/core';
 import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
-import { FormControl, FormGroup, ControlLabel, HelpBlock} from 'react-bootstrap';
-
+import InfiniteCalendar from 'react-infinite-calendar';
+import { FormControl, FormGroup, ControlLabel, HelpBlock, DropdownButton, MenuItem, InputGroup} from 'react-bootstrap';
+import 'react-infinite-calendar/styles.css';
 export class CourseCreate extends BaseComponent {
 
     constructor(props) {
         super(props);
         this.state = {
+            anchorEl: null,
         };
+    }
+
+    handleClick = event => {
+        this.setState({
+            anchorEl: event.currentTarget,
+        });
+    };
+
+    handleClose = () => {
+        this.setState({
+            anchorEl: null,
+        });
+    };
+
+    onClickCreate = () => {
+        alert(this.state.startDate);
+    }
+
+    handleDate = (date) => {
+        this.setState({startDate: date})
     }
 
 
     renderTextInput = (name, variable) => {
+        return (
+            <Grid style={styles.inputContainer} container>
+                <Grid xs={3} style={styles.textContainer} container>
+                    <Typography style={styles.text}>{name} :</Typography>
+                </Grid>
+                <Grid xs={9} style={styles.textContinaer} container>
+                    <FormControl type="text" onChange={this.handleChange(variable)} />
+                </Grid>
+            </Grid>
+        )
+    }
+
+    
+
+    renderDateInput = (name, variable) => {
         return (
             <Grid style={styles.inputContainer} container>
                 <Grid xs={3} style={styles.textContainer} container>
@@ -41,83 +78,56 @@ export class CourseCreate extends BaseComponent {
                     {this.renderTextInput("Name", "name")}
                     {this.renderTextInput("Location", "location")}
                     {this.renderTextInput("Professor", "professorName")}
-                    {this.renderTextInput("Introduction", "introduction")}
                     {this.renderTextInput("Credit", "credit")}
                     {this.renderTextInput("Avatar", "avatar")}
-                    {this.renderTextInput("Start Date", "avatar")}
-                    {this.renderTextInput("End Date", "avatar")}
                 </Grid>
 
                 <Grid xs={8} contaienr>
-
-                    <TextField
-                        id="time"
-                        label="Alarm clock"
-                        type="datetime-local"
-                        defaultValue="07:30"
-                
-                        InputLabelProps={{
-                            shrink: true,
+                    <Button variant="contained" onClick={this.handleClick}>
+                        Open Popover
+                    </Button>
+                    <Popover
+                        open={Boolean(this.state.anchorEl)}
+                        anchorEl={this.state.anchorEl}
+                        onClose={this.handleClose}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
                         }}
-                        inputProps={{
-                            step: 300, // 5 min
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'center',
                         }}
-                    />
+                    >
+                        <Typography>The content of the Popover.</Typography>
+                        <InfiniteCalendar
+                            displayOptions={{
+                                showHeader: false,
+                                shouldHeaderAnimate: false,
+                                showTodayHelper: false,
+                            }}
+                            onSelect={this.handleDate}
+                        />
+                    </Popover>
 
-                    <TextField
-                        defaultValue="react-bootstrap"
-                        id="bootstrap-input"
-                        InputProps={{
-                            disableUnderline: true,
-                            style: {
-                                root: styles.bootstrapRoot,
-                                input: styles.bootstrapInput,
-                            },
-                        }}
-                    />
+                    {this.renderTextInput("Start Date", "avatar")}
+                    {this.renderTextInput("End Date", "avatar")}
+                    {this.renderTextInput("Introduction", "introduction")}
+                   
+               
 
-                            
 
+                    <DropdownButton componentClass={InputGroup.Button} id="input-dropdown-addon" title="Action">
+                        <MenuItem key="1">Item
+                        </MenuItem>
+                    </DropdownButton>
+
+                    
+ 
                 </Grid>
 
-
-
-                <Grid style={{margin:'10px'}}>
-                    <Typography variant='title'>Course Location</Typography>
-                </Grid>
-                <Grid style={{margin:'10px'}} direction='row' alignItems="flex-end" container>  
-                    <Grid xs={2}>
-                        <TextField
-                        label="Campus District"
-                        style={styles.input}
-                        onChange={this.handleChange('campus')}
-                        fullWidth/>
-                        {/*should be replace with selector*/}
-                    </Grid>
-                    <Grid style={{marginLeft:'10px'}} xs={3}>
-                        <TextField
-                        label="Building"
-                        style={styles.input}
-                        onChange={this.handleChange('building')}
-                        fullWidth/>
-                    </Grid>
-                    <Grid style={{marginLeft:'10px'}} xs={3}>
-                        <TextField
-                        label="Room Number"
-                        style={styles.input}
-                        onChange={this.handleChange('room')}
-                        fullWidth/>
-                    </Grid>
-                </Grid>
-                <Grid style={{margin:'10px'}} direction='row' container>
-                    <Typography variant='title'>What kind of course will it be?</Typography>
-                    <Input 
-                    style={styles.input}
-                    onChange={this.handleChange('kind')}/>
-                </Grid>
-                <Divider light/><br/>
-                <Grid>
-                    <Button>Create Class</Button>
+                <Grid xs={12}>
+                    <Button onClick={this.onClickCreate}>Create Class</Button>
                 </Grid>
             </Grid>
         );
@@ -146,6 +156,11 @@ const styles = {
     text: {
         fontSize: '20px',
         marginRight: '10px',
+    },
+
+    test: {
+        width: '300px',
+        height: '400px'
     }
 
 
