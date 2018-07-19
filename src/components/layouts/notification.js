66fc56a35be2bdf4177a8ae444f53b49519d05ae
Winning12 <wenhao_jun@outlook.com>
2018-07-19 11:39:53 +0800

    update coursepanel

--- src/components/layouts/notification.js
+++ src/components/layouts/notification.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { hide_notification } from '../../redux/actions/action'
 import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+
 
 
 const mapStateToProps = state => ({
@@ -17,14 +19,30 @@ class Notification extends Component {
         this.props.dispatch(hide_notification());
     }
 
+    handleContent = () => {
+        if(this.props.kind=='danger'){
+            return(
+            <SnackbarContent
+                style={{backgroundColor:'red'}}
+                message={<span>{this.props.reason}</span>}
+            />)
+        }else
+            return(
+            <SnackbarContent
+                style={{backgroundColor:'black'}}
+                message={<span>{this.props.reason}</span>}
+            />)
+        //add your type of content here
+    }
+
     render() {
         return (
             <Snackbar
-                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
                 open={this.props.open}
-                onClose={this.handleClose}
-                message={<span>{this.props.reason}</span>}
-            />
+                onClose={this.handleClose}>
+                {this.handleContent()}
+            </Snackbar>
         );
     }
 }
