048b4fd1cb11ae3d7c4ed6924aa6312efe686138
OASans <40996687+OASans@users.noreply.github.com>
2018-08-09 00:10:48 +0800

    Update courseCreate.js

--- src/screens/course/courseCreate.js
+++ src/screens/course/courseCreate.js
@@ -1,193 +1,92 @@
-import React, { Component } from "react";
+import React from 'react';
+import { Row, Col, Input, Button, Icon, Form, Upload, Avatar } from 'antd';
 import { BaseComponent } from '../../components/BaseComponent';
-import { Divider, Grid, Button, Typography, Input, TextField, Popover} from '@material-ui/core';
-import { FormControl, FormGroup, ControlLabel, HelpBlock, DropdownButton, MenuItem, InputGroup, Textarea} from 'react-bootstrap';
-import 'react-infinite-calendar/styles.css';
-import ImageUploader from 'react-images-upload';
+import {FormButton, FormText, FormAvatar, FormSelector} from '../../components';
+
 
 export class CourseCreate extends BaseComponent {
 
     constructor(props) {
         super(props);
         this.state = {
-            name: null,
-            code: null,
-            introduction: null,
-            credit: null,
-            avatar: [],
+            loading: false,
+            avatarUrl: null,
         };
+
     }
 
-    onClickCreate = () => {
-        this.createCourse()
-    }
-
-    createCourse = () => {
-
-        let form = new FormData();
-        form.append("name", this.state.name);
-        form.append("type", this.state.type);
-        form.append("faculty", this.state.faculty);
-        form.append("code", this.state.code);
-        form.append("introduction", this.state.introduction);
-        form.append("credit", this.state.credit);
-        form.append('avatar', this.state.avatar[0]);
-        console.log(this.state.avatar)
-        this.post('/api/course/create', form).then((result) => {
-
-            if (!result) {
-                this.pushNotification("danger", "Connection error", this.props.dispatch);
-
-            } else if (result.status === 'fail') {
-                this.pushNotification("danger", result.status, this.props.dispatch);
-
-            } else if (result.status === 'success') {
-
-                this.pushNotification("success", "successfully create the course", this.props.dispatch);
-
-            } else {
-                alert(JSON.stringify(result))
-                this.pushNotification("danger", result.status, this.props.dispatch);
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
             }
 
-        })
-    }
+            let form = new FormData();
+            form.append('name', values.name);
+            form.append('code', values.code);
+            form.append('type', values.type);
+            form.append('credit', values.credit);
+            form.append('avatar', values.avatar);
+            form.append('campus', values.campus);
+            form.append('faculty', values.faculty);
+            form.append('period', values.period);
+            form.append('resource', values.resource);
+            form.append('reference', values.reference);
+            form.append('target', values.target);
+            form.append('content', values.content);
+            form.append('other', values.other);
 
-    renderTextInput = (name, variable) => {
-        return (
-            <Grid style={styles.inputContainer} container>
-                <Grid xs={3} style={styles.textContainer} container>
-                    <Typography style={styles.text}>{name} :</Typography>
-                </Grid>
-                <Grid xs={9} style={styles.textContinaer} container>
-                    <FormControl type="text" onChange={this.handleChange(variable)} />
-                </Grid>
-            </Grid>
-        )
-    }
+            var successAction = (result) => {
+                this.props.history.goBack();
+                this.pushNotification("success", "successfully create the course");
+            }
 
-    renderIntroduction = (name, variable) => {
-        return (
-            <Grid style={styles.inputContainer} container>
-                <Grid xs={3} style={styles.textContainer} container>
-                    <Typography style={styles.text}>{name} :</Typography>
-                </Grid>
-                <Grid xs={9} container>
-                    <FormControl componentClass="textarea" onChange={this.handleChange(variable)} multilple style={styles.introContainer} rows={4}/>
-                </Grid>
-            </Grid>
-        )
-    }
+            this.newPost('/api/course/create', form, successAction);
 
-    renderChooseAvatar = () => {
-
-        var onChange = (avatar) => {
-     
-                console.log(avatar);
-        
-     
-            this.setState({
-                avatar: avatar
-            });
-            
-        } 
-
-        return (
-            <Grid style={styles.inputContainer} container>
-                <Grid xs={3} style={styles.textContainer} container>
-                    <Typography style={styles.text}>Avatar: </Typography>
-                </Grid>
-                <Grid xs={9} container>
-                    <ImageUploader
-                        withIcon={false}
-                        withLabel={false}
-                        withPreview={true}
-                        buttonText='Choose images'
-                        buttonStyle={styles.uploadButton}
-                        onChange={onChange}
-                        imgExtension={['.jpg', '.gif', '.png']}
-                        maxFileSize={5242880}
-                        singleImage={true}
-                    />
-                </Grid>
-            </Grid>
-        )
+        });
     }
 
     render() {
+
         return (
-            <Grid justify='center' container>
+            <Row type='flex' justify='center'>
+                <Col span={20}>
+                    <Form onSubmit={this.handleSubmit}>
 
-                <Grid xs={12} style={styles.headerContainer}>
-                    <Typography variant='display2'>New Course</Typography>
-                </Grid>
+                        <FormText form={this.props.form} label='Name' name='name' required={true}/>
 
-                <Grid xs={12} container>
+                        <FormText form={this.props.form} label='Code' name='code' required={true} />
 
-                    {this.renderTextInput("Code", "code")}
+                        <FormText form={this.props.form} label='Type' name='type' required={true} />
 
-                    {this.renderTextInput("Name", "name")}
+                        <FormText form={this.props.form} label='Credit' name='credit' required={true} />
 
-                    {this.renderTextInput("Type", "type")}
+                        <FormText form={this.props.form} label='Campus' name='campus' required={true} />
 
-                    {this.renderTextInput("Faculty", "faculty")}
+                        <FormText form={this.props.form} label='Faculty' name='faculty' required={true} />
 
-                    {this.renderTextInput("Credit", "credit")}
+                        <FormText form={this.props.form} label='Period' name='period' required={true} />
 
-                    {this.renderChooseAvatar()}
+                        <FormText form={this.props.form} label='Resource' name='resource' required={true} />
 
-                    {this.renderIntroduction("Introduction", "introduction")}
+                        <FormText form={this.props.form} label='Reference' name='reference' required={true} />
 
-                </Grid>
+                        <FormText form={this.props.form} label='Target' name='target' required={true} />
 
-     
+                        <FormText form={this.props.form} label='Content' name='content' required={true} />
 
-                <Grid xs={12}>
-                    <Button onClick={this.onClickCreate}>Create Class</Button>
-                </Grid>
-                
-            </Grid>
+                        <FormText form={this.props.form} label='Other' name='other' required={true} />
+
+                        <FormAvatar form={this.props.form}/>
+
+                        <FormButton form={this.props.form} label="submit" />
+
+                    </Form>
+                </Col>
+            </Row>
         );
     }
 }
 
-
-const styles = {
-
-    headerContainer: {
-        marginBottom: '20px'
-    },
-
-    inputContainer: {
-        marginBottom: '20px'
-    },
-
-    input: {
-        borderRadius: '6px'
-    },
-
-    textContainer: {
-        justifyContent: 'flex-end'
-    },
-
-    text: {
-        fontSize: '20px',
-        marginRight: '10px',
-    },
-
-    test: {
-        width: '300px',
-        height: '400px'
-    },
-
-    introContainer: {
-        height: '80px'
-    },
-
-    uploadButton: {
-        height: '200px'
-    }
-
-
-};
 
