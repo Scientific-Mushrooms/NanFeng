5a31c553cb8403cc2df930717707becde68f7313
OASans <40996687+OASans@users.noreply.github.com>
2018-08-20 21:46:53 +0800

    Update classroomLayout.js

--- src/screens/classroom/classroomLayout.js
+++ src/screens/classroom/classroomLayout.js
@@ -1,14 +1,50 @@
 import React, {Component} from "react";
+import { BaseComponent } from '../../components/BaseComponent';
 import { Switch, Route, Redirect } from "react-router-dom";
-import {classroomRoutes} from '../../routes/routes'
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {classroomRoutes} from '../../routes/routes';
 import { Row, Col,Button,Layout,Breadcrumb,Divider,Menu,Icon} from 'antd';
 import ClassroomList from './instructorPanel/components/classroomList';
 import AssignmentList from './instructorPanel/components/assignmentList';
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
-export class ClassroomLayout extends Component {
-    
+export class ClassroomLayout extends BaseComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            avatarUrl: null,
+            classrooms: [],
+        };
+    }
+
+    componentWillMount=()=>{
+
+        var instructorId = sessionStorage.getItem('instructorId');
+
+        if (instructorId === null) {
+            this.props.history.push("/home")
+            return;
+        }
+
+        this.fetchClassrooms(instructorId);
+    }
+
+    fetchClassrooms = (instructorId) => {
+        let form = new FormData();
+        form.append("instructorId", instructorId);
+
+        var successAction = (result) => {
+            this.setState({classrooms: result.detail});
+            this.pushNotification("success", "successfully fetch classrooms", this.props.dispatch);
+        }
+
+        this.newPost('/api/classroom/instructorIdToAllClassrooms', form, successAction);
+
+    }
 
     createRoutes = (routes) => {
         return (
@@ -18,19 +54,39 @@ export class ClassroomLayout extends Component {
         )
     };
 
+    renderCard = (classroom, index) => {
+
+        var goToClassroom = (classroomId) => () => {
+            this.props.history.push('/classroom/classroomPanel/' + classroomId);
+        }
+
+        return (
+            <Menu.Item  onClick={goToClassroom(classroom.classroomId)}>
+                <div>
+                    <Row style={styles.classroomTitle}>{classroom.name}</Row>
+                </div>
+            </Menu.Item>
+        )
+    }
+
     render() {
         return (
             <Row justify='center' type='flex'>
-                <Col span={22}>
+                <Col span={24}>
                     <Layout style={{ padding: '0 0', background: '#fff' }}>
-                        <Sider  width={250} style={{ background: '#fff' }}>
+                        <Sider  width={230} style={{ background: '#fff' }}>
                             <Menu
                                 mode="inline"
-                                defaultSelectedKeys={['1']}
                                 defaultOpenKeys={['sub1']}
-                                style={{ height: '100%', backgroundColor:'#eff5f6'}}>
+                                style={{ height: '100%'}}>
+                                <Menu.Item onClick={() => { this.props.history.push('/classroom/classroomCreate') }}>
+                                    <Icon type="plus" />新建课堂
+                                </Menu.Item>
+                                <Menu.Item onClick={() => { this.props.history.push('/classroom/classroomDelete') }}>
+                                    <Icon type="minus" />删除课堂
+                                </Menu.Item>
                                 <SubMenu key="sub1" title={<span><Icon type="switcher" />课堂列表</span>}>
-                                    <ClassroomList/>
+                                    {this.state.classrooms.map(this.renderCard)}
                                 </SubMenu>
                                 <SubMenu key="sub2" title={<span><Icon type="book" />作业列表</span>}>
                                     <AssignmentList/>
@@ -48,5 +104,16 @@ export class ClassroomLayout extends Component {
         );
     }
 }
+
+const styles = {
+
+    button:{
+        height:'40px',
+        weight:'180px',
+    }
+
+}
+
+
 
 
